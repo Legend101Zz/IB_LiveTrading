5a747fb3a6d49ee4fa9b163fd93e4ab399f94d57
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2024-03-22 02:15:47 +0530

    added event code to handle and listen for pending ticker events

--- ib_test.py
+++ ib_test.py
@@ -4,6 +4,8 @@ from ib_insync import *
 ib = IB()
 ib.connect('127.0.0.1', 7497, clientId=1)
 
+## ========= TO GET HISTORICAL DATA ========= ##
+
 contract = Forex('EURUSD')
 bars = ib.reqHistoricalData(
     contract, endDateTime='', durationStr='30 D',
@@ -11,4 +13,18 @@ bars = ib.reqHistoricalData(
 
 # convert to pandas dataframe (pandas needs to be installed):
 df = util.df(bars)
-print(df) 
+# print(df) 
+
+market_data =ib.reqMktData(contract,'',False,False ) 
+
+
+print(market_data)
+
+
+def onPendingTickers(tickers):
+    print("Pending ticker event received")
+    print(tickers)
+
+ib.pendingTickersEvent += onPendingTickers
+
+ib.run()
